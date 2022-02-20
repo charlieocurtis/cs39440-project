9d10e30732b5c0609df21a686d19520047a42b75
xaviersays <xaviersays@protonmail.com>
2022-02-20 14:27:04 +0000

    Added the reading of BMP to generate list of pixel RGB values

---
+++ src/png_to_bmp_test/main.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+# collect the png file
+png_input = "test_image.png"
+image = Image.open(png_input)
+
+# convert the png into bmp
+bmp_output = "test_image.bmp"
+image.save(bmp_output)
+
+# collect the bmp
+bmp_collection = Image.open(bmp_output)
+# collect each pixel rgb values and put into list
+bmp_pix_vals = list(bmp_collection.getdata())
+
+# write each tuple to the output file ready to be read and compared with original
+with open("output.txt", "w") as file:
+    for rgb in bmp_pix_vals:
+        file.write(str(rgb))
+        file.write("\n")
+
+
