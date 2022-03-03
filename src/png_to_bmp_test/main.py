cd9d1b6212d869d07a0323ef87e4fe15bf9b4b18
xaviersays <xaviersays@protonmail.com>
2022-03-03 11:36:09 +0000

    Updated script to collect luminance values to enable calculation of MSE

--- src/png_to_bmp_test/main.py
+++ src/png_to_bmp_test/main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 
 # collect the png file
 png_input = "test_image.png"
@@ -19,5 +20,24 @@ with open("output.txt", "w") as file:
     for rgb in bmp_pix_vals:
         file.write(str(rgb)[1:-1])
         file.write("\n")
+
+# converts image to grey scale and then collects luminance values
+# can now calculate MSE
+luminance = np.asarray(bmp_collection.convert("L"))
+
+counter = 0
+
+# counter is for debugging purposes when trying to establish
+# how the library is outputting data into the given text file
+with open("luminance-out.txt", "w") as file:
+    for line in luminance:
+        file.write(str(line))
+        file.write("\n")
+        counter += 1
+
+# when printing luminance values for bmp version of image
+# the correct dimensions are printed to text file
+print(counter)
+# print(f"{luminance}\n")
 
 
