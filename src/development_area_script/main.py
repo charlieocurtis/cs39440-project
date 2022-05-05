a9b0e86ce2ff0faf049149364e7b1fd197f2efbf
xaviersays <xaviersays@protonmail.com>
2022-05-05 19:18:12 +0100

    Fixes (#10)

--- src/development_area_script/main.py
+++ src/development_area_script/main.py
@@ -12,12 +12,25 @@ np.set_printoptions(threshold=sys.maxsize)
 
 
 def find_mode(subject_image):
-    # find the current mode of the required image
+    """
+    Find the current mode of the required image
+
+    :param subject_image: The image that will have its mode checked
+    :type subject_image: String
+    :return: The PIL defined 'mode' of the image
+    """
     image = Image.open(subject_image)
     return image.mode
 
 
 def check_colour_depth(subject_image):
+    """
+    Check the bit-depth of a given image
+
+    :param subject_image: The image that will have bit-depth checked
+    :type subject_image: String
+    :return: Boolean corresponding to 16 and 8 bit
+    """
     # load an image
     subject = Image.open(subject_image)
 
@@ -30,6 +43,13 @@ def check_colour_depth(subject_image):
 
 
 def collect_greyscale_pixels(target_image):
+    """
+    Function to collect the individual pixel values for a given image
+
+    :param target_image: The image that will have its pixel values collected
+    :type target_image: String
+    :return: Numpy array with size equal to resolution of target_image containing individual pixel values
+    """
     image = Image.open(target_image)
 
     # if 8bit image run standard collection procedure
@@ -44,36 +64,73 @@ def collect_greyscale_pixels(target_image):
 
 
 def convert_16bit_to_8bit(image_array):
+    """
+    Function to convert a 16-bit image to an 8-bit image
+
+    :param image_array: An array of individual pixel values
+    :type image_array: Array
+    :return: An array oif individual pixel values between 0 and 255
+    """
     # function to handle converting 16bit pixel range (0 - 65536)
     # to 8bit range (0-255)
     return (image_array * 1) / 256
 
 
 def write_pixel_values(image_array, save_file):
+    """
+    Function to write an array of pixel values to a file
+
+    :param image_array: The array containing individual pixel values of an image
+    :param save_file: The name of the file to be written to
+    """
     # debugging purposes
-    # writing collected image pixel vals to text file
     with open(save_file, 'w') as file:
         file.write(str(image_array))
 
 
 def calculate_mse(perfect_reference_image, subject_image):
-    # function to calculate Mean Squared Error
+    """
+    Function to calculate the mean sqaured error of two images
+
+    :param perfect_reference_image: The original 'perfect' reference image
+    :param subject_image: The compressed image to have mse calculated for
+    :return: The mean squared error of the compressed image
+    """
     return mean_squared_error(check_colour_depth(perfect_reference_image),
                               check_colour_depth(subject_image))
 
 
 def calculate_psnr(perfect_reference_image, subject_image):
-    # function to calculate Peak Signal to Noise Ratio
+    """
+    Function to calulate the peak signal to noise ratio between two images
+
+    :param perfect_reference_image: The original 'perfect' reference image
+    :param subject_image: The compressed image to have psnr calculated for
+    :return: The peak signal to noise ratio of the compressed image
+    """
     # peak signal val = 255 for 8bit, and 65536 for 16bit
     return (10 * (np.log10(65536 ** 2))) / calculate_mse(perfect_reference_image, subject_image)
 
 
 def calculate_ssim(perfect_reference_image, subject_image):
-    # function to calculate Structural Similarity Index
+    """
+    Function to calculate the Structural Similarity Index between two images
+
+    :param perfect_reference_image: The original 'perfect' reference image
+    :param subject_image: The copressed image to have SSIM calculated for
+    :return: The SSIM of the compressed image
+    """
     return ssim(perfect_reference_image, subject_image)
 
 
 def calculate_difference(original_image, new_image):
+    """
+    Function to calculate the absolute difference between two arrays of pixel values
+
+    :param original_image: An array of pixel values from the original image
+    :param new_image: An array of pixel values from the compressed image
+    :return: A third array containing the absolute difference between original_image and new_image
+    """
     # load an original image
     original_pixel_vals = np.array(collect_greyscale_pixels(original_image))
     # and a compressed version
@@ -92,6 +149,13 @@ def calculate_difference(original_image, new_image):
 
 
 def load_raw_image(raw_image_file):
+    """
+    Function to generate array of individual pixel values from raw data file of an image
+    usually in .dat format
+
+    :param raw_image_file: The raw image file to be loaded
+    :return: Numpy array of individual pixel values
+    """
     image = np.fromfile(raw_image_file, dtype=np.uint16)
     # filler = np.zeros(shape=(1, 33), dtype=np.uint16)
     # image = np.append(arr=image, values=filler)
@@ -100,6 +164,11 @@ def load_raw_image(raw_image_file):
 
 
 def calculate_averages():
+    """
+    Function to calculate and display the averages for difference in file sizes of original
+    and compressed image variants
+    """
+    # load data from csv for bpe and j2k
     bpe_values = pd.read_csv("bpe_data_csv.csv", usecols=[2, 3, 8, 13, 18], names=["original-size",
                                                                                    "compressed-size-8",
                                                                                    "decompressed-size-8",
@@ -111,6 +180,7 @@ def calculate_averages():
                                                                            "compressed-size-80"],
                              skiprows=2, skip_blank_lines=True, dtype=np.float64).dropna()
 
+    # calculate the differences in file sizes at each compression level for bpe and j2k
     size_difference_bpe_8x_compressed = np.array(bpe_values['original-size']) - \
                                         np.array(bpe_values['compressed-size-8'])
     size_difference_bpe_80x_compressed = np.array(bpe_values['original-size']) - \
@@ -123,6 +193,7 @@ def calculate_averages():
     size_difference_j2k_8x = np.array(j2k_values['original-size'] - j2k_values['compressed-size-8'])
     size_difference_j2k_80x = np.array(j2k_values['original-size'] - j2k_values['compressed-size-80'])
 
+    # print the results
     print(f"=====BPE=====\n"
           f"Average file size difference at:\n"
           f"8x compression: {int(np.average(size_difference_bpe_8x_compressed))}\n"
@@ -145,15 +216,22 @@ def calculate_averages():
 
 
 def generate_box(algorithm):
-    # For boxplots of real time at both compression ratios for BPE and j2k
+    """
+    Function to generate and display boxplots from MatPlotLib of desired data
+
+    :param algorithm: The condition for the if staement (either BPE or J2K depending on which boxplot is required)
+    """
     if algorithm.upper().strip() == "BPE":
+        # load data
         bpe_values = pd.read_csv("bpe_data_csv.csv", usecols=[2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22], names=[
             "original-size", "compress-size-8", "compress-time-8", "compress-mse-8", "compress-psnr-8",
             "compress-ssim-8", "compress-size-80", "compress-time-80", "compress-mse-80", "compress-psnr-80",
             "compress-ssim-80"], skiprows=3, skip_blank_lines=True, dtype=np.float64).dropna()
+        # process data
         size_difference_bpe_8x_compressed = np.array(bpe_values["original-size"] - bpe_values["compress-size-8"])
         size_difference_bpe_80x_compressed = np.array(bpe_values['original-size'] - bpe_values['compress-size-80'])
         plt.boxplot(size_difference_bpe_80x_compressed, labels=["File size difference from CCSDS 122.0-B-2 80x compression (MB)"])
+        # draw box
         plt.show()
     elif algorithm.upper().strip() == "J2K":
         bpe_values = pd.read_csv("bpe_data_csv.csv", usecols=[2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22], names=[
@@ -175,6 +253,10 @@ def generate_box(algorithm):
 
 
 def generate_scatter():
+    """
+    Function to generate scatter graphs for data representation
+    """
+    # load data
     bpe_values = pd.read_csv("bpe_data_csv.csv", usecols=[2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22], names=[
         "original-size", "compress-size-8", "compress-time-8", "compress-mse-8", "compress-psnr-8", "compress-ssim-8",
         "compress-size-80", "compress-time-80", "compress-mse-80", "compress-psnr-80", "compress-ssim-80"],
@@ -184,43 +266,57 @@ def generate_scatter():
         "compress-size-80", "compress-time-80", "compress-mse-80", "compress-psnr-80", "compress-ssim-80"],
                              skiprows=2, skip_blank_lines=True, dtype=np.float64).dropna()
 
+    # all scatters of difference in file size (original and compressed) against...
     size_difference_bpe_8x_compressed = np.array(bpe_values["original-size"] - bpe_values["compress-size-8"])
     size_difference_bpe_80x_compressed = np.array(bpe_values['original-size'] - bpe_values['compress-size-80'])
     size_difference_j2k_8x_compressed = np.array(j2k_values["original-size"] - j2k_values["compress-size-8"])
     size_difference_j2k_80x_compressed = np.array(j2k_values["original-size"] - j2k_values["compress-size-80"])
 
+    # plot scatter
     plt.scatter(j2k_values["compress-mse-8"], j2k_values["compress-psnr-8"], c="blue")
     plt.xlabel("MSE")
     plt.ylabel("PSNR")
     plt.show()
     # CCSDS 122.0-B-2
 
-    # print(stats.pearsonr(size_difference_bpe_8x_compressed, bpe_values["compress-time-8"]))
-    # print(stats.pearsonr(size_difference_bpe_80x_compressed, bpe_values["compress-time-80"]))
-    # print(stats.pearsonr(size_difference_j2k_8x_compressed, j2k_values["compress-time-8"]))
-    # print(stats.pearsonr(size_difference_j2k_80x_compressed, j2k_values["compress-time-80"]))
+
+def print_stats():
+    """
+    Function to print the computed stats found in the report
+    """
+    # load data
+    bpe_values = pd.read_csv("bpe_data_csv.csv", usecols=[2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22], names=[
+        "original-size", "compress-size-8", "compress-time-8", "compress-mse-8", "compress-psnr-8", "compress-ssim-8",
+        "compress-size-80", "compress-time-80", "compress-mse-80", "compress-psnr-80", "compress-ssim-80"],
+                             skiprows=3, skip_blank_lines=True, dtype=np.float64).dropna()
+    j2k_values = pd.read_csv("j2k_data_csv.csv", usecols=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], names=[
+        "original-size", "compress-size-8", "compress-time-8", "compress-mse-8", "compress-psnr-8", "compress-ssim-8",
+        "compress-size-80", "compress-time-80", "compress-mse-80", "compress-psnr-80", "compress-ssim-80"],
+                             skiprows=2, skip_blank_lines=True, dtype=np.float64).dropna()
+
+    # calculate size differences
+    size_difference_bpe_8x_compressed = np.array(bpe_values["original-size"] - bpe_values["compress-size-8"])
+    size_difference_bpe_80x_compressed = np.array(bpe_values['original-size'] - bpe_values['compress-size-80'])
+    size_difference_j2k_8x_compressed = np.array(j2k_values["original-size"] - j2k_values["compress-size-8"])
+    size_difference_j2k_80x_compressed = np.array(j2k_values["original-size"] - j2k_values["compress-size-80"])
+
+    # pearsons correlation coefficient
+    print(stats.pearsonr(size_difference_bpe_8x_compressed, bpe_values["compress-time-8"]))
+    print(stats.pearsonr(size_difference_bpe_80x_compressed, bpe_values["compress-time-80"]))
+    print(stats.pearsonr(size_difference_j2k_8x_compressed, j2k_values["compress-time-8"]))
+    print(stats.pearsonr(size_difference_j2k_80x_compressed, j2k_values["compress-time-80"]))
 
     # print(max(bpe_values["compress-time-8"]))
     # print(max(bpe_values["compress-time-8"]) - min(bpe_values["compress-time-80"]))
-    #
+
     # print(max(j2k_values["compress-time-8"]))
     # print(max(j2k_values["compress-time-8"]) - min(j2k_values["compress-time-8"]))
 
-    # print(stats.ttest_ind(bpe_values["compress-size-80"], j2k_values["compress-size-80"]))
+    # two sample t-test
+    print(stats.ttest_ind(bpe_values["compress-size-80"], j2k_values["compress-size-80"]))
 
 
 def main():
-    # print(find_mode("AUPE_images_for_compression_tests/acli_sandpit_rocks/pre-compression/comptest_objects2-sandpit2_LWAC01_T00_P00.png"))
-    # print(check_colour_depth("sample_images/LWAC01_compression_10.j2k"))
-    # print(np.array(Image.open("sample_images/LWAC01_compression_10.j2k").getdata()))
-    # generated_image = np.asarray(calculate_difference("test_image.png",
-    #                                                   "sample_images/comptest_objects2-sandpit2_LWAC01_T00_P00.png"))
-    # created_image = Image.fromarray(generated_image)
-    # print(calculate_psnr(generated_image))
-    # created_image.show()
-    # image = "sample_images/comptest_objects2-sandpit2_LWAC01_T00_P00.png"
-    # find_mode(image)
-
     difference_vals = calculate_difference("AUPE_images_for_compression_tests/ACE_outside_2xpct/pre-compression/P00_T00_L01_00.png",
                                           "AUPE_images_for_compression_tests/ACE_outside_2xpct/j2k_compress/P00_T00_L01_00_80.j2k")
     print(np.max(difference_vals))
@@ -230,51 +326,6 @@ def main():
     # calculate_averages()
     # generate_box("j2k")
     # generate_scatter()
-
-
-    # image = Image.fromarray(load_raw_image(
-    #     "collated/acli_sandpit_rocks/compress_0.1/comptest_objects2-sandpit2_RWAC01_T00_P00_compressed_0.1.decoded.dat"))
-    # image.show()
-    # image.save("collated/acli_sandpit_rocks/compress_0.1/comptest_objects2-sandpit2_RWAC01_T00_P00_compressed_0.1.decoded.png")
-
-    # print(load_raw_image("sample_images/LWAC01_outfile_compressed.dat"))
-
-    # print("MSE: ")
-    # print(calculate_mse("AUPE_images_for_compression_tests/ACE_outside_2xpct/pre-compression/P00_T00_L01_00.png",
-    #                     "collated/ACE_outside_2xpct/compress_0.1/P00_T00_L01_00_compressed_0.1.decoded.png"))
-    # print("PSNR: ")
-    # print(calculate_psnr('AUPE_images_for_compression_tests/ACE_outside_2xpct/pre-compression/P00_T00_L01_00.png',
-    #                      'collated/ACE_outside_2xpct/compress_0.1/P00_T00_L01_00_compressed_0.1.decoded.png'))
-    # print("SSIM:")
-    # print(calculate_ssim(
-    #     check_colour_depth('AUPE_images_for_compression_tests/ACE_outside_2xpct/pre-compression/P00_T00_L01_00.png'),
-    #     check_colour_depth("collated/ACE_outside_2xpct/compress_0.1/P00_T00_L01_00_compressed_0.1.decoded.png")))
-
-    # print(collect_greyscale_pixels("LWAC01_JPG_to_PNG.png"))
-    # print(convert_16bit_to_8bit(collect_greyscale_pixels_16bit("sample_images/comptest_objects2-sandpit2_LWAC01_T00_P00.png")))
-    # png_to_bmp("LWAC01_JPG_to_PNG.png", "LWAC01_JPG_to_BMP.bmp")
-    # print(find_mode("L0123-Composite.png"))
-
-    # write_pixel_values(collect_greyscale_pixels("LWAC01_JPG_to_BMP.bmp"), "test-output.txt")
-
-    # print(calculate_difference("LWAC01_JPG.jpg", "sample_images/comptest_objects2-sandpit2_LWAC01_T00_P00.png"))
-
-    # sample_array = calculate_difference("sample_images/test_j2k_compress_out.j2k", "sample_images/comptest_objects2-sandpit2_LWAC01_T00_P00.png")
-    # sample_image = Image.fromarray(sample_array, mode='L')
-    # sample_image.show()
-    # print(sample_array)
-
-    # print(collect_greyscale_pixels("saved-image.png"))
-    # convert_image_format("LWAC02_JPG.jpg", "LWAC02_JPG_to_PNG.png")
-
-    # print(calculate_mse("LWAC01_JPG_to_PNG.png", "LWAC01_JPG_to_BMP.bmp"))
-
-    # test_vals = collect_luminance_vals_8bit("test_image.png")
-    # write_luminance_vals(LUMINANCE_WRITE, test_vals)
-    # png_to_jpg_8bit("test_image.png", "test_image.jpg")
-    # write_luminance_vals("jpg-luminance-out.txt", collect_luminance_vals_8bit("test_image.jpg"))
-
-    # convert_grey_8bit_png("test_image.png")
 
 
 if __name__ == '__main__':
